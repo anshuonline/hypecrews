12de7aba9053a9529f577d1d9c1bf736a1a0f804
anshuonline <helloanshu.dev@gmail.com>
2025-12-05 19:32:16 +0530

    Update client logos section with real company logos and create logo directory structure

--- index.php
+++ index.php
@@ -473,18 +473,13 @@ $pageTitle = "Hypecrews - Professional Digital Services";
             </div>
             
             <div class="animate-slide">
-                <!-- Google -->
+                <!-- Axis Bank -->
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                                <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
-                                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-                            </svg>
+                            <img src="graphics/logos/axis-bank.png" alt="Axis Bank" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">Google</span>
+                        <span class="text-2xl font-bold text-white">Axis Bank</span>
                     </div>
                 </div>
                 
@@ -492,36 +487,39 @@ $pageTitle = "Hypecrews - Professional Digital Services";
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#FF9900" d="M10.13 17.76c-.1-.15-.13-.33-.13-.53v-1.11h3.63v.53c0 .2-.03.38-.13.53-.1.15-.28.25-.48.25h-2.4c-.2 0-.38-.1-.48-.25zm-3.63-5.49c0-.2.03-.38.13-.53.1-.15.28-.25.48-.25h2.4c.2 0 .38.1.48.25.1.15.13.33.13.53v1.11h-3.63v-.53zm12.13 5.49c-.1-.15-.13-.33-.13-.53v-1.11h3.63v.53c0 .2-.03.38-.13.53-.1.15-.28.25-.48.25h-2.4c-.2 0-.38-.1-.48-.25zm-3.63-5.49c0-.2.03-.38.13-.53.1-.15.28-.25.48-.25h2.4c.2 0 .38.1.48.25.1.15.13.33.13.53v1.11h-3.63v-.53z"/>
-                                <path fill="#146EB4" d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"/>
-                            </svg>
+                            <img src="graphics/logos/amazon.png" alt="Amazon" class="w-8 h-8 object-contain">
                         </div>
                         <span class="text-2xl font-bold text-white">Amazon</span>
                     </div>
                 </div>
                 
-                <!-- Facebook -->
+                <!-- Decathlon -->
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#1877F2" d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-                            </svg>
+                            <img src="graphics/logos/decathlon.png" alt="Decathlon" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">Facebook</span>
+                        <span class="text-2xl font-bold text-white">Decathlon</span>
                     </div>
                 </div>
                 
-                <!-- YouTube -->
+                <!-- Samsung -->
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#FF0000" d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
-                            </svg>
+                            <img src="graphics/logos/samsung.png" alt="Samsung" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">YouTube</span>
+                        <span class="text-2xl font-bold text-white">Samsung</span>
+                    </div>
+                </div>
+                
+                <!-- Infosys -->
+                <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
+                    <div class="flex items-center">
+                        <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
+                            <img src="graphics/logos/infosys.png" alt="Infosys" class="w-8 h-8 object-contain">
+                        </div>
+                        <span class="text-2xl font-bold text-white">Infosys</span>
                     </div>
                 </div>
                 
@@ -529,14 +527,9 @@ $pageTitle = "Hypecrews - Professional Digital Services";
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                                <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
-                                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-                            </svg>
+                            <img src="graphics/logos/axis-bank.png" alt="Axis Bank" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">Google</span>
+                        <span class="text-2xl font-bold text-white">Axis Bank</span>
                     </div>
                 </div>
                 
@@ -544,36 +537,39 @@ $pageTitle = "Hypecrews - Professional Digital Services";
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#FF9900" d="M10.13 17.76c-.1-.15-.13-.33-.13-.53v-1.11h3.63v.53c0 .2-.03.38-.13.53-.1.15-.28.25-.48.25h-2.4c-.2 0-.38-.1-.48-.25zm-3.63-5.49c0-.2.03-.38.13-.53.1-.15.28-.25.48-.25h2.4c.2 0 .38.1.48.25.1.15.13.33.13.53v1.11h-3.63v-.53zm12.13 5.49c-.1-.15-.13-.33-.13-.53v-1.11h3.63v.53c0 .2-.03.38-.13.53-.1.15-.28.25-.48.25h-2.4c-.2 0-.38-.1-.48-.25zm-3.63-5.49c0-.2.03-.38.13-.53.1-.15.28-.25.48-.25h2.4c.2 0 .38.1.48.25.1.15.13.33.13.53v1.11h-3.63v-.53z"/>
-                                <path fill="#146EB4" d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"/>
-                            </svg>
+                            <img src="graphics/logos/amazon.png" alt="Amazon" class="w-8 h-8 object-contain">
                         </div>
                         <span class="text-2xl font-bold text-white">Amazon</span>
                     </div>
                 </div>
                 
-                <!-- Facebook -->
+                <!-- Decathlon -->
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#1877F2" d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-                            </svg>
+                            <img src="graphics/logos/decathlon.png" alt="Decathlon" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">Facebook</span>
+                        <span class="text-2xl font-bold text-white">Decathlon</span>
                     </div>
                 </div>
                 
-                <!-- YouTube -->
+                <!-- Samsung -->
                 <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
                     <div class="flex items-center">
                         <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-                                <path fill="#FF0000" d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
-                            </svg>
+                            <img src="graphics/logos/samsung.png" alt="Samsung" class="w-8 h-8 object-contain">
                         </div>
-                        <span class="text-2xl font-bold text-white">YouTube</span>
+                        <span class="text-2xl font-bold text-white">Samsung</span>
+                    </div>
+                </div>
+                
+                <!-- Infosys -->
+                <div class="flex-shrink-0 flex items-center justify-center p-8 bg-light rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 w-64 inline-flex mx-4">
+                    <div class="flex items-center">
+                        <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center mr-4">
+                            <img src="graphics/logos/infosys.png" alt="Infosys" class="w-8 h-8 object-contain">
+                        </div>
+                        <span class="text-2xl font-bold text-white">Infosys</span>
                     </div>
                 </div>
             </div>
