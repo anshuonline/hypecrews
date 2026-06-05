50f8c44b8c81d8912712dac7f905bf180cfca717
anshuonline <helloanshu.dev@gmail.com>
2026-06-05 15:40:05 +0530

    Fixed session_start notice in api_chat.php

---
+++ adminstrator/test_api.php
@@ -0,0 +1,7 @@
+<?php
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_GET['chat_with'] = 'group';
+session_start();
+$_SESSION['admin_logged_in'] = true;
+$_SESSION['admin_id'] = 1;
+include 'api_chat.php';
