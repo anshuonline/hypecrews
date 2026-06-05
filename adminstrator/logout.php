9aa3970a35b1212237958575e493e4646931b77e
anshuonline <helloanshu.dev@gmail.com>
2026-06-05 15:26:41 +0530

    Added activity logging for admin logout

--- adminstrator/logout.php
+++ adminstrator/logout.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+require_once '../config/db.php';
+require_once 'components/logger.php';
+
+if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
+    logAdminActivity($pdo, 'LOGOUT', 'Admin logged out.');
+}
+
 session_destroy();
 header('Location: login.php');
 exit;
