4c9631d82cbc12dc64e6dfd28eec11c5d75be475
anshuonline <helloanshu.dev@gmail.com>
2026-06-05 15:40:18 +0530

    Removed test scripts

--- test_api.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_SERVER['REQUEST_METHOD'] = 'GET';
-$_GET['chat_with'] = 'group';
-session_start();
-$_SESSION['admin_logged_in'] = true;
-$_SESSION['admin_id'] = 1;
-include 'adminstrator/api_chat.php';
